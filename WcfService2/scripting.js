e4e855ab100df39e0f59f820ebaf64cfec08f16b
indorekm <indorekm@mail.uc.edu>
2018-11-25 09:30:00 -0500

    Project Files Uploaded

---
+++ WcfService2/scripting.js
@@ -0,0 +1,30 @@
+﻿function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("menuIcon").style.display = "none";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("menuIcon").style.display = "block";
+}
+
+(function () {
+
+    var myIndex = 0;
+    carousel();
+
+    function carousel() {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        myIndex++;
+        if (myIndex > x.length) { myIndex = 1 }
+        x[myIndex - 1].style.display = "block";
+        setTimeout(carousel, 2000); // Change image every 2 seconds
+    }
+
+})();
